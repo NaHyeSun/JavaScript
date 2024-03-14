69135f1002523e4e7a4a8d565a59bac1aa6e180b
NaHyeSun <tpdus8487@gmail.com>
2024-03-14 15:43:14 +0900

    .

--- 01_core/08_prototype/02_function-prototype/01_object-constructor-prototype.js
+++ 01_core/08_prototype/02_function-prototype/01_object-constructor-prototype.js
@@ -1,4 +1,6 @@
 /* 1. 생성자 함수 프로토타입 */
+// new 연산자를 사용해 만든 객체는
+// 생성자 함수의 프로토타입 정보를 사용해 [[Prototype]]을 설정한다.
 
 const user = {
     activate: true,
@@ -16,6 +18,7 @@ Student.prototype = user;
 
 // F.prototype은 new F를 호출할 때만 사용된다. 
 // new F를 호출할 때 만들어지는 새로운 객체의 [[Prototype]]을 할당한다.
+// student.__proto__ == user
 let student = new Student('판다');
 
 console.log(student.activate);
